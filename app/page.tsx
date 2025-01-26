f3bd538df34461e04af00949723fbe46f9d1d406
ZakariaBatty <zbatty1297@gmail.com>
2025-01-26 15:57:30 +0100

    Uncomment RegionSidebar component in the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import { MoroccoMap } from "@/components/morocco-map"
-// import { RegionSidebar } from "@/components/region-sidebar"
+import { RegionSidebar } from "@/components/region-sidebar"
 
 export default function Home() {
   return (
     <div className="flex">
-      {/* <RegionSidebar /> */}
-      {/* <main className="flex-1 p-8">
-        <div className="max-w-5xl mx-auto space-y-8"> */}
       <MoroccoMap />
-      {/* </div>
-      </main> */}
     </div>
   )
 }
